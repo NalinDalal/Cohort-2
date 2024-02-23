2dba9349b607a933b89d2f1b87a8c70fb8220646
NalinDalal <nalindalal2004@gmail.com>
2024-02-23 14:14:18 +0530

    backend done along with maybe u know databses

--- week_5/ToDo_2/backend/index.js
+++ week_5/ToDo_2/backend/index.js
@@ -3,12 +3,13 @@
 
 const express = require('express');
 const {createTodo}=require("./types");
+const {todo}=require("./db");
 const app = express();
 
 app.use(express.json());
 
 //validation
-app.post("/todo",function(req,res){
+app.post("/todo",async function(req,res){
     const createPayload=req.body;
     const parsedPayload=createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
@@ -18,14 +19,25 @@ app.post("/todo",function(req,res){
         return;
     }
     //put it in mongodb
+    await todo.create({
+        title:createPayload.title,
+        description:createPayload.description,
+        completed:false
+    })
+
+    res.json({
+        msg:"Todo created successfully",
+    });
 })
 
-app.get("/todos", function(req, res) {
-  
+app.get("/todos", async function(req, res) {
+  const todos=await todo.find({});
+  // console.log(todos)    //promise
+  res.json({todos})
 });
 
 //mongodb schema updating
-app.put("/completed",function(req,res){
+app.put("/completed",async function(req,res){
     const updatePayload=req.body;
     const parsedPayload=updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
@@ -34,9 +46,14 @@ app.put("/completed",function(req,res){
         });
         return;
     }
-    //update in mongodb
+    //update in mongodb,todo having _id mark it as completed
+    await todo.updateOne({_id:req.body.id},{
+        completed:true
+    })
+    res.json({msg:"todo marked as completed"})
 })
 
 // app.listen(port, () => {
 //   console.log(`Server is running at http://localhost:${port}`);
 // });
+app.listen(3000);
