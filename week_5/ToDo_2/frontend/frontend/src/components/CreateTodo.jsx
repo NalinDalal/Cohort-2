0c4c5b6abb7e2a7f93706f78a5de712864002ae4
NalinDalal <nalindalal2004@gmail.com>
2024-02-23 14:57:24 +0530

    frontend

---
+++ week_5/ToDo_2/frontend/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="title"></input> <br></br>
+        <input type="text" placeholder="description"></input> <br></br>
+
+        <button>Add a Todo</button>
+    </div>
+}
