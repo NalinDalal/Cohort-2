ec73e3c1daa9afaf384ffa07f43fab5d2a5513d2
NalinDalal <nalindalal2004@gmail.com>
2024-03-14 23:24:55 +0530

    ports updated

--- week_5/ToDo_2/frontend/src/App.jsx
+++ week_5/ToDo_2/frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CreateTodo } from './components/CreateTodo'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { CreateTodo } from "./components/CreateTodo";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todo")
-  .then(async function(res){
-    const json=await res.json();
-    setTodos(json.todos);
-  })
+  fetch("http://localhost:5173/todo")
+    .then(async function (res) {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
 
   return (
     <div>
       <CreateTodo></CreateTodo>
       <Todos todos={todos}></Todos>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
